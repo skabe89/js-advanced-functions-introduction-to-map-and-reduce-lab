1f87392293681061a94b75e634b4e8494ba6c4e7
Sean <skabe89@gmail.com>
2021-02-24 10:21:26 -0500

    commit tree 72db18cc361fb18dd69cb5c4f1744a2f605d5ebe

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+let mapToNegativize = function(sourceArray){
+  let newArray = []
+  for(let i = 0; i < sourceArray.length; i++){
+    newArray.push(sourceArray[i] * -1)
+  }
+  return newArray
+}
+
+let mapToNoChange = function(sourceArray){
+  let newArray = []
+  for(let i = 0; i < sourceArray.length; i++){
+    newArray.push(sourceArray[i] * -1)
+  }
+  return sourceArray
+}
+
+let mapToDouble = function(sourceArray){
+  let newArray = []
+  for(let i = 0; i < sourceArray.length; i++){
+    newArray.push(sourceArray[i] * 2)
+  }
+  return newArray
+}
+
+let mapToSquare = function(sourceArray){
+  let newArray = []
+  for(let i = 0; i < sourceArray.length; i++){
+    newArray.push(sourceArray[i] * sourceArray[i])
+  }
+  return newArray
+}
+
+let reduceToTotal = function(sourceArray, start = 0){
+  let running = start
+  for(let i = 0; i < sourceArray.length; i++){
+    running = running + sourceArray[i]
+  }
+  return running
+}
+
+let reduceToAllTrue = function(sourceArray){
+  let value = true
+  for(let i = 0; i < sourceArray.length; i ++){
+    if (sourceArray[i] == false){
+      value = false
+    }
+  }
+  return value
+}
+
+let reduceToAnyTrue = function(sourceArray){
+  let value = false
+  for(let i = 0; i < sourceArray.length; i ++){
+    if (sourceArray[i] == true){
+      value = true
+    }
+  }
+  return value
+}
